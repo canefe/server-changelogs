6f6b9c9acc6bdd0ed925bb64606a45c5b7393d62
canefe <turkiyecraft@gmail.com>
2017-10-06 18:42:11 +0300

    Update 2.1
    Some fixes.

--- server-changelog-on-login/lua/scol/cl_menu.lua
+++ server-changelog-on-login/lua/scol/cl_menu.lua
@@ -104,3 +104,155 @@ function PANEL:Paint(w,h)
 end
 
 vgui.Register("flatblurButton", PANEL, "DButton")
+
+local PANEL = {}
+function PANEL:Init()
+
+    local PanelCategory = self
+
+    PanelCategory.Children = {}
+    PanelCategory.IsToggled = false
+
+    PanelCategory:SetTall( 35 )
+    PanelCategory.Button = vgui.Create( "DButton", PanelCategory )
+    PanelCategory.Button:Dock( TOP )
+    PanelCategory.Button:DockMargin( 5, 5, 5, 0 )
+    PanelCategory.Button:SetHeight( 25 )
+    PanelCategory.Button:SetText( "" )
+
+    PanelCategory.Button.DoClick = function() PanelCategory:DoToggle() end
+
+    PanelCategory.Button.Paint = function(self, w, h)
+        draw.RoundedBox( 0, 0, 0, w, h, Color( 5, 5, 5, 200 ) )
+        draw.SimpleText( PanelCategory.Title, "Trebuchet24", 5, 12, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
+    end
+
+end
+
+-----------------------------------------------------------------
+-- [ PAINT ]
+-- Better than microsoft paint
+-----------------------------------------------------------------
+
+function PANEL:Paint(w, h)
+    draw.RoundedBox(0, 1, 0, w - 2, h, Color(5, 5, 5, 200))
+end
+
+-----------------------------------------------------------------
+-- [ ADD NEW CHILD ]
+-- Add new entries
+-----------------------------------------------------------------
+function PANEL:AddNewChild(element)
+    local PanelCategory = self
+
+    if not IsValid(element) then return end
+    table.insert(PanelCategory.Children, element)
+
+    --PanelCategory.List:PerformLayout()
+end
+
+-----------------------------------------------------------------
+-- [ SETUP CHILDREN ]
+-- Do a little math calc to determine the height of the cat panel
+-----------------------------------------------------------------
+
+function PANEL:SetupChildren()
+    local PanelCategory = self
+    PanelCategory:SetTall( 25 + PanelCategory.List:GetTall() + 15 )
+end
+-----------------------------------------------------------------
+-- [ TOGGLE OPENED ]
+-- Action when category list is toggled open
+-----------------------------------------------------------------
+
+function PANEL:ToggleOpened()
+    local PanelCategory = self
+    PanelCategory.IsToggled = true
+    PanelCategory:SizeTo( PanelCategory:GetWide(), 25 + table.Count(PanelCategory.Children) * 200, 0.5, 0.1 )
+end
+
+-----------------------------------------------------------------
+-- [ TOGGLE CLOSED ]
+-- Action when category list is toggled closed
+-----------------------------------------------------------------
+
+function PANEL:ToggleClosed()
+    local PanelCategory = self
+    PanelCategory.IsToggled = false
+    PanelCategory:SizeTo( PanelCategory:GetWide(), 35, 0.5, 0.1 )
+end
+
+-----------------------------------------------------------------
+-- [ DO TOGGLE ]
+-- Determine state of category list and perform next state
+-----------------------------------------------------------------
+
+function PANEL:DoToggle()
+    local PanelCategory = self
+    if PanelCategory.IsToggled then
+        PanelCategory:ToggleClosed()
+    else
+        PanelCategory:ToggleOpened()
+    end
+end
+
+-----------------------------------------------------------------
+-- [ HEADER TITLE ]
+-- Set the category title in header
+-----------------------------------------------------------------
+
+function PANEL:HeaderTitle(catTitle)
+    local PanelCategory = self
+    PanelCategory.Title = catTitle
+end
+
+vgui.Register("scolCategory", PANEL, "DPanel")
+
+PANEL = {}
+
+function PANEL:Init()
+    self.Icons = {}
+    self.IconMaterials = {}
+end
+
+function PANEL:SetText(value)
+    -- add some space for the icon
+    -- this is really not rigorous
+    self.BaseClass.SetText(self, "     " .. value)
+    self:SetTextColor( Color( 255, 255, 255 ) )
+end
+
+function PANEL:AddChoice(value, icon, data, select)
+    local choiceIndex = self.BaseClass.AddChoice(self, value, data, select)
+    self.Icons[choiceIndex] = icon or false
+    self.IconMaterials[choiceIndex] = icon and Material(icon) or false
+    return choiceIndex
+end
+
+
+function PANEL:PaintOver(w, h)
+    local selected = self:GetSelectedID()
+    local icon = self.IconMaterials[selected]
+    if not icon then return end
+    local iconSize = 16
+    local padding = h/2 - iconSize/2
+    surface.SetDrawColor(color_white)
+    surface.SetMaterial(icon)
+    surface.DrawTexturedRect(padding, padding, iconSize, iconSize)
+end
+
+function PANEL:Paint(w,h)
+    draw.RoundedBox(0, 0, 0, w, h, Color(44, 62, 80, 180))
+end
+
+function PANEL:OpenMenu()
+    self.BaseClass.OpenMenu(self)
+    local items = self.Menu:GetCanvas():GetChildren()
+    for index, item in ipairs(items) do
+        local icon = self.Icons[index]
+        if not icon then continue end
+        item:SetIcon(icon)
+    end
+end
+
+vgui.Register("DComboBoxIcons", PANEL, "DComboBox")
